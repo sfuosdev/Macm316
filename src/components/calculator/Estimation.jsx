b422460abc601976796efbc5111790d0aa141b61
pisache <hobulhojunlee20@gmail.com>
2023-06-19 18:20:42 -0700

    ADD: trapezodial estimation component

--- src/components/calculator/Estimation.jsx
+++ src/components/calculator/Estimation.jsx
@@ -1,12 +1,35 @@
 import React from 'react';
-import LaTex from './Latex';
+import useCalculatorState from '../../hooks/useCalculatorState';
+import {
+    differentiationMethods,
+    integrationMethods,
+} from '../../context/constants';
+import TrapezodialEstimation from './TrapezoidalEstimation';
+
+function MethodSwitch() {
+    const [state] = useCalculatorState();
+
+    function forMethod() {
+        switch (state.method) {
+            case differentiationMethods.MIDDLE_POINT:
+                return <>Midpoint Differentiation</>;
+            case differentiationMethods.LAGRANGE_POLYNOMIAL_THREE_POINT:
+                return <>Lagrange</>;
+            case integrationMethods.MIDPOINT_RULE:
+                return <>Midpoint Integration</>;
+            case integrationMethods.TRAPEZOIDAL_RULE:
+                return TrapezodialEstimation();
+            case integrationMethods.SIMPSON_RULE:
+                return <>Simpson</>;
+            default:
+                return null;
+        }
+    }
+    return forMethod();
+}
 
 function Estimation() {
-    return (
-        <div className="Formula">
-            <LaTex tex="f(x) = e^{x}" />
-        </div>
-    );
+    return MethodSwitch();
 }
 
 export default Estimation;
---
+++ src/components/calculator/TrapezoidalEstimation.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import LaTex from './Latex';
+
+function TrapezodialEstimation() {
+    return (
+        <div className="Formula">
+            <LaTex tex="f(x) = e^{x}" />
+        </div>
+    );
+}
+
+export default TrapezodialEstimation;
