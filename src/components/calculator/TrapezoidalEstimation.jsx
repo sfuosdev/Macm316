86ba8e5f537742ab51a047ad02e53838a1ccc005
pisache <hobulhojunlee20@gmail.com>
2023-06-23 10:44:21 -0700

    UPDATE: cases for Math.PI

--- src/components/calculator/TrapezoidalEstimation.jsx
+++ src/components/calculator/TrapezoidalEstimation.jsx
@@ -2,19 +2,18 @@ import React from 'react';
 import trapezoidal from '../../lib/trapezoidal';
 import LaTex from './Latex';
 
-function TrapezodialEstimation(func, xStart, xEnd, interval) {
-    const estimation = trapezoidal(func, xStart, xEnd, interval).toFixed(6);
-
-    const x0 = (xStart + 0).toFixed(0);
-    const xn = (xEnd + 0).toFixed(0);
-    const delta = (xEnd - xStart) / interval;
-    const h = 2 * interval;
-    const x1 = (xStart + delta).toFixed(1);
-    const x2 = (xEnd - delta).toFixed(1);
-
+function LatexString(
+    func,
+    x0,
+    x1,
+    x2,
+    xn,
+    h,
+    interval,
+    estimation,
+    latexString,
+) {
     if (interval > 3) {
-        const latexString = `\\scriptsize\\int_{${x0}}^{${xn}}f(x)dx\\approx\\frac{(${xn}-${x0})}{${h}}\\left[f(${x0})+2f(${x1})+\\cdots +2f(${x2})+f(${xn})\\right]`;
-
         return (
             <div className="Estimation">
                 <p>Formula</p>
@@ -31,8 +30,8 @@ function TrapezodialEstimation(func, xStart, xEnd, interval) {
             </div>
         );
     }
-
-    const latexString = `\\scriptsize\\int_{${x0}}^{${xn}}f(x)dx\\approx\\frac{(${xn}-${x0})}{${h}}\\left[f(${x0})+2f(${x1})+2f(${x2})+f(${xn})\\right]`;
+    let latex = latexString;
+    latex = `\\scriptsize\\int_{${x0}}^{${xn}}f(x)dx\\approx\\frac{(${xn}-${x0})}{${h}}\\left[f(${x0})+2f(${x1})+2f(${x2})+f(${xn})\\right]`;
 
     return (
         <div className="Estimation">
@@ -46,8 +45,92 @@ function TrapezodialEstimation(func, xStart, xEnd, interval) {
             <LaTex tex="\tiny\int_{a}^{b}f(x)dx\approx\frac{(b-a)}{2n}\left[f(x_{0})+2f(x_{1})+2f(x_{2})+\cdots +2f(x_{n-1})+f(x_{n})\right]" />
             <br />
             <p>Actual Estimation</p>
-            <LaTex tex={latexString} />
+            <LaTex tex={latex} />
         </div>
+    );
+}
+
+function PIString(
+    piAtStart,
+    func,
+    x0,
+    x1,
+    x2,
+    xn,
+    h,
+    interval,
+    estimation,
+    latexString,
+) {
+    if (piAtStart === true) {
+        return LatexString(
+            func,
+            x0,
+            x1,
+            x2,
+            xn,
+            h,
+            interval,
+            estimation,
+            latexString,
+        );
+    }
+    return LatexString(
+        func,
+        x0,
+        x1,
+        x2,
+        xn,
+        h,
+        interval,
+        estimation,
+        latexString,
+    );
+}
+
+function TrapezodialEstimation(func, xStart, xEnd, interval) {
+    const estimation = trapezoidal(func, xStart, xEnd, interval).toFixed(6);
+
+    const x0 = (xStart + 0).toFixed(0);
+    const xn = (xEnd + 0).toFixed(0);
+    const delta = (xEnd - xStart) / interval;
+    const h = 2 * interval;
+    const x1 = (xStart + delta).toFixed(1);
+    const x2 = (xEnd - delta).toFixed(1);
+
+    let latexString = `\\scriptsize\\int_{${x0}}^{${xn}}f(x)dx\\approx\\frac{(${xn}-${x0})}{${h}}\\left[f(${x0})+2f(${x1})+\\cdots +2f(${x2})+f(${xn})\\right]`;
+    let isPI = false;
+
+    if (xStart !== Math.PI && xEnd !== Math.PI) {
+        return LatexString(
+            func,
+            x0,
+            x1,
+            x2,
+            xn,
+            h,
+            interval,
+            estimation,
+            latexString,
+        );
+    }
+    if (xStart === Math.PI) {
+        latexString = `\\scriptsize\\int_{\\pi}^{${xn}}f(x)dx\\approx\\frac{(${xn}-\\pi)}{${h}}\\left[f(\\pi)+2f(${x1})+\\cdots +2f(${x2})+f(${xn})\\right]`;
+        isPI = true;
+    } else if (xEnd === Math.PI) {
+        latexString = `\\scriptsize\\int_{${x0}}^{\\pi}f(x)dx\\approx\\frac{(\\pi-${x0})}{${h}}\\left[f(${x0})+2f(${x1})+\\cdots +2f(${x2})+f(\\pi)\\right]`;
+    }
+    return PIString(
+        isPI,
+        func,
+        x0,
+        x1,
+        x2,
+        xn,
+        h,
+        interval,
+        estimation,
+        latexString,
     );
 }
 
